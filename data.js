aab82992a045f4c424e60ce53e482c7e14db85f5
Geomark SIG Solutions <maumerobin38@gmail.com>
2026-04-30 18:14:46 +0400

    Add DATA object with software and service details

---
+++ data.js
@@ -0,0 +1,106 @@
+export const DATA = {
+  logiciels: {
+    qgis: {
+      n: "QGIS",
+      d: "SIG open source complet",
+      p: 0,
+      img: "Qgis.png"
+    },
+    arcgis: {
+      n: "ArcGIS Enterprise",
+      d: "Suite SIG complète ESRI",
+      p: 4500,
+      img: "ArcGisPRO.jpg"
+    },
+    autocad: {
+      n: "AutoCAD Map 3D",
+      d: "SIG orienté DAO",
+      p: 2500,
+      img: "Autocad.jpg"
+    }
+  },
+
+  bases: {
+    postgis: {
+      n: "PostGIS",
+      d: "Base spatiale robuste",
+      p: 0,
+      img: "PostGre_PostGIS.jpg"
+    },
+    supabase: {
+      n: "Supabase",
+      d: "PostGIS managé",
+      p: 300,
+      img: "Supabase.png"
+    },
+    gpkg: {
+      n: "GeoPackage",
+      d: "Stockage local",
+      p: 0,
+      img: "Geopackage.jpg"
+    }
+  },
+
+  serveurs: {
+    ovh: {
+      n: "OVHcloud VPS",
+      d: "Cloud souverain",
+      p: 120,
+      img: "OVH.png"
+    },
+    aws: {
+      n: "AWS",
+      d: "Cloud scalable",
+      p: 1200,
+      img: "Amazon_Web_Services.png"
+    },
+    local: {
+      n: "Serveur local",
+      d: "Infrastructure interne",
+      p: 0,
+      img: "Local.png"
+    }
+  },
+
+  web: {
+    lizmap: {
+      n: "Lizmap",
+      d: "Web SIG QGIS",
+      p: 0,
+      img: "Lizmap.png"
+    },
+    openlayers: {
+      n: "OpenLayers",
+      d: "Lib JS carto",
+      p: 0,
+      img: "Openlayers.png"
+    },
+    experience: {
+      n: "Experience Builder",
+      d: "Apps ArcGIS",
+      p: 0,
+      img: "Experience Builder.jpg"
+    }
+  },
+
+  imagerie: {
+    osm: {
+      n: "OSM / Google",
+      d: "Fond gratuit",
+      p: 0,
+      img: "OSM.jpg"
+    },
+    sentinel: {
+      n: "Sentinel / Planet",
+      d: "Imagerie satellite",
+      p: 450,
+      img: "sentinel-2.jpg"
+    },
+    drone: {
+      n: "Drone",
+      d: "Très haute précision",
+      p: 1500,
+      img: "Drones.jpg"
+    }
+  }
+};
